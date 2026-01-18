0fc363d4ccb167cc02ec2884a4c36d87d4f6f96a
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2026-01-19 03:17:39 +0600

    add Post Parcel data and create parcel get api

--- index.js
+++ index.js
@@ -30,12 +30,21 @@ async function run() {
 
     //   parcel api
     app.get("/parcels", async (req, res) => {
+      const query = {};
+      const { email } = req.query;
+      if (email) {
+        query.senderEmail = email;
+      }
+      const cursor = parcelsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/parcels", async (req, res) => {
       const parcel = req.body;
       const result = parcelsCollection.insertOne(parcel);
       res.send(result);
     });
-
-    app.post("/parcels", async (req, res) => {});
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
