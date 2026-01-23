a8e90ed312e0aaad7256be86aa481349623c4c3d
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2026-01-23 23:09:12 +0600

    add id based parcel api

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      
+      const result = await parcelsCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/parcels", async (req, res) => {
       const parcel = req.body;
       const result = await parcelsCollection.insertOne(parcel);
