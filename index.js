3c283071c55880d31fb335197bbf048e2a08068d
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2026-01-27 16:05:51 +0600

    comment await client

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const parcelDB = client.db("zapShiftDB");
     const parcelsCollection = parcelDB.collection("parcels");
